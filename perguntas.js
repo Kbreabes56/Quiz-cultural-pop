9ec8471d0cc352d2995c0e3a5554104b106d8cb3
Kbreabes56 <kbreanes@gmail.com>
2025-06-25 16:39:54 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,33 @@
+const perguntasArray = [
+
+   {
+    icone:"filmes e series.jpg",
+    pergunta: "Em 'Matrix', qual é o nome real de Neo?",
+    opcoes: ["Thomas A. Anderson", "John Wick", "Morpheus", "Smith"],
+    resposta: "Thomas A. Anderson"
+  },
+  {
+    icone:"jogo.png",
+    pergunta: "No jogo 'The Legend of Zelda: Majora's Mask', quantos dias Link tem para salvar o mundo?",
+    opcoes: ["5", "3", "7", "10"],
+    resposta: "3"
+  },
+    {
+    icone:"filmes e series.jpg",
+    pergunta: "Em 'Star Wars', qual é o planeta natal do Chewbacca?",
+    opcoes: ["Naboo", "Kashyyyk", "Endor", "Hoth"],
+    resposta: "Kashyyyk"
+  },
+  {
+    icone:"filmes e series.jpg",
+    pergunta: "Na série 'Stranger Things', qual é o nome completo da personagem Eleven?",
+    opcoes: ["Jane Hopper", "Eleven Wheeler", "Jane Byers", "Sara Hopper"],
+    resposta: "Jane Hopper"
+  },
+  {
+    icone:"japa.jpg",
+    pergunta: "Qual editora publica o mangá 'One Piece'?",
+    opcoes: ["Kodansha", "Shueisha", "Square Enix", "Kadokawa"],
+    resposta: "Shueisha"
+  }  
+];
